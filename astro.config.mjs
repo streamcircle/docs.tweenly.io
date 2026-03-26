ca50c5f2c854567de86f6e0d22f75357e215f213
“Martina <martina@brezinova.eu>
2026-03-26 13:37:39 +0100

    Squashed commit of the following:
    commit 2b24857a6c15085db999fe31640c045fd8b76ec9
    Author: “Martina <“martina@brezinova.eu”>
    Date:   Thu Mar 26 13:36:15 2026 +0100
    
        Add Editor v2.1.5 release notes
    
    commit 3fd2b2c8265901dfe000073c21655ffeaf1d2f47
    Author: “Martina <“martina@brezinova.eu”>
    Date:   Wed Jan 14 15:43:42 2026 +0100
    
        Change theme colors
    
    commit e28160aa574c6ed20689f5d3828c6cad235796f9
    Author: “Martina <“martina@brezinova.eu”>
    Date:   Tue Jan 13 11:32:39 2026 +0100
    
        Change TWEENLY DOCS for logo
    
    commit b5d000314eb8f1170ef0f8afa747e3cf040fcccd
    Author: “Martina <“martina@brezinova.eu”>
    Date:   Tue Jan 13 11:18:02 2026 +0100
    
        Change Roboto for Manrope, Inter
    
    commit 01205d9582a55efc992724b5e8a21c00db5e82d1
    Author: “Martina <“martina@brezinova.eu”>
    Date:   Tue Jan 13 11:17:38 2026 +0100
    
        Change homepage for introduction, remove "splash" page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,122 +4,98 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://docs.tweenly.io",
-  integrations: [
-    starlight({
-      title: "Tweenly",
-      logo: {
-        light: "./src/assets/logo_tweenly_light.svg",
-        dark: "./src/assets/logo_tweenly_dark.svg",
-        alt: "Tweenly",
-        replacesTitle: true,
-      },
-      social: {
-        facebook: "https://www.facebook.com/app.tween.ly/",
-        linkedin: "https://www.linkedin.com/company/tween-ly/",
-        youtube: "https://www.youtube.com/@Tweenly",
-      },
-      sidebar: [
-        {
-          label: "Getting Started",
-          items: [
-            { label: "Installation", link: "/getting-started/installation/" },
-            {
-              label: "Manage License",
-              link: "/getting-started/manage-license/",
+    site: "https://docs.tween.ly",
+    integrations: [
+        starlight({
+            title: "Tweenly",
+            logo: {
+                light: "./src/assets/logo_tweenly_light.svg",
+                dark: "./src/assets/logo_tweenly_dark.svg",
+                alt: "Tweenly",
+                replacesTitle: true,
             },
-            {
-              label: "Wizard Tutorial",
-              link: "/getting-started/wizard-tutorial/",
+            social: {
+                facebook: "https://www.facebook.com/app.tween.ly/",
+                linkedin: "https://www.linkedin.com/company/tween-ly/",
+                youtube: "https://www.youtube.com/@Tweenly",
             },
-            {
-              label: "Export Options",
-              link: "/getting-started/export-options/",
-            },
-            // { label: "Create Your First Graphics", link: "/getting-started/create-your-first-graphics/" },
-          ],
-        },
-        {
-          label: "Main Features",
-          items: [
-            { label: "Tickers", link: "/main-features/tickers/" },
-            {
-              label: "Image sequences",
-              link: "/main-features/image-sequences/",
-            },
-            { label: "Code triggers", link: "/main-features/code-triggers/" },
-            {
-              label: "Data source & API control",
-              link: "/main-features/data-source-api-control/",
-            },
-            {
-              label: "Built-in Test App",
-              link: "/main-features/built-in-test-app/",
-            },
-          ],
-        },
-        {
-          label: "User Guide",
-          items: [
-            {
-              label: "Application Overview",
-              link: "/user-guide/application-overview",
-            },
-            { label: "Editor", link: "/user-guide/editor-overview/" },
-            { label: "Gallery", link: "/user-guide/gallery-overview/" },
-            { label: "Workspaces", link: "/user-guide/workspaces-overview/" },
-            { label: "Profile", link: "/user-guide/profile-overview/" },
-            { label: "Graphics Files", link: "/user-guide/graphics-files/" },
-            { label: "Assets", link: "/user-guide/assets/" },
-            {
-              label: "Keyboard shortcuts",
-              link: "/user-guide/keyboard-shortcuts/",
-            },
-          ],
-        },
-        // {
-        //     label: "User Guide",
-        //     autogenerate: { directory: "user-guide" },
-        // },
-        {
-          label: "Release Notes",
-          collapsed: true,
-          items: [
-            {
-              label: "v1.12.0",
-              link: "/release-notes/v1120",
-              badge: { text: "current", variant: "success" },
-            },
-            { label: "v1.11.0", link: "/release-notes/v1110" },
-            { label: "v1.10.2", link: "/release-notes/v1102" },
-            { label: "v1.10.1", link: "/release-notes/v1101" },
-            { label: "v1.9.3", link: "/release-notes/v193" },
-            { label: "v1.9.2", link: "/release-notes/v192" },
-            { label: "v1.9.0", link: "/release-notes/v190" },
-            { label: "v1.8.0", link: "/release-notes/v180" },
-            { label: "v1.7.0", link: "/release-notes/v170" },
-            { label: "v1.6.1", link: "/release-notes/v161" },
-            { label: "v1.6.0", link: "/release-notes/v160" },
-            { label: "v1.5.0", link: "/release-notes/v150" },
-            { label: "v1.4.4", link: "/release-notes/v144" },
-            { label: "v1.4.3", link: "/release-notes/v143" },
-            { label: "v1.4.2", link: "/release-notes/v142" },
-            { label: "v1.4.1", link: "/release-notes/v141" },
-            { label: "v1.4.0", link: "/release-notes/v140" },
-          ],
-        },
-        {
-          label: "Appendices",
-          autogenerate: { directory: "appendices" },
-        },
-        {
-          label: "Support & Contact",
-          link: "/support-and-contact/",
-        },
-      ],
-      // customCss: ["@fontsource/roboto", "./src/tailwind.css"],
-      customCss: ["@fontsource/inter", "./src/tailwind.css"],
-    }),
-    tailwind({ applyBaseStyles: false }),
-  ],
+            sidebar: [
+                {
+                    label: "Getting Started",
+                    items: [
+                        { label: "Installation", link: "/getting-started/installation/" },
+                        { label: "Manage License", link: "/getting-started/manage-license/" },
+                        { label: "Wizard Tutorial", link: "/getting-started/wizard-tutorial/" },
+                        { label: "Export Options", link: "/getting-started/export-options/" },
+                        // { label: "Create Your First Graphics", link: "/getting-started/create-your-first-graphics/" },
+                    ],
+                },
+                {
+                    label: "Main Features",
+                    items: [
+                        { label: "Tickers", link: "/main-features/tickers/" },
+                        { label: "Image sequences", link: "/main-features/image-sequences/" },
+                        { label: "Code triggers", link: "/main-features/code-triggers/" },
+                        { label: "Data source & API control", link: "/main-features/data-source-api-control/" },
+                        { label: "Built-in Test App", link: "/main-features/built-in-test-app/" },
+                    ],
+                },
+                {
+                    label: "User Guide",
+                    items: [
+                        { label: "Application Overview", link: "/user-guide/application-overview" },
+                        { label: "Editor", link: "/user-guide/editor-overview/" },
+                        { label: "Gallery", link: "/user-guide/gallery-overview/" },
+                        { label: "Workspaces", link: "/user-guide/workspaces-overview/" },
+                        { label: "Profile", link: "/user-guide/profile-overview/" },
+                        { label: "Graphics Files", link: "/user-guide/graphics-files/" },
+                        { label: "Assets", link: "/user-guide/assets/" },
+                        { label: "Keyboard shortcuts", link: "/user-guide/keyboard-shortcuts/" },
+                    ],
+                },
+                // {
+                //     label: "User Guide",
+                //     autogenerate: { directory: "user-guide" },
+                // },
+                {
+                    label: "Release Notes",
+                    collapsed: true,
+                    items: [
+                        {
+                            label: "v2.1.5", link: "/release-notes/v215",
+                            badge: { text: "current", variant: "success" }
+                        },
+                        { label: "v1.12.0", link: "/release-notes/v1120" },
+                        { label: "v1.11.0", link: "/release-notes/v1110" },
+                        { label: "v1.10.2", link: "/release-notes/v1102" },
+                        { label: "v1.10.1", link: "/release-notes/v1101" },
+                        { label: "v1.9.3", link: "/release-notes/v193" },
+                        { label: "v1.9.2", link: "/release-notes/v192" },
+                        { label: "v1.9.0", link: "/release-notes/v190" },
+                        { label: "v1.8.0", link: "/release-notes/v180" },
+                        { label: "v1.7.0", link: "/release-notes/v170" },
+                        { label: "v1.6.1", link: "/release-notes/v161" },
+                        { label: "v1.6.0", link: "/release-notes/v160" },
+                        { label: "v1.5.0", link: "/release-notes/v150" },
+                        { label: "v1.4.4", link: "/release-notes/v144" },
+                        { label: "v1.4.3", link: "/release-notes/v143" },
+                        { label: "v1.4.2", link: "/release-notes/v142" },
+                        { label: "v1.4.1", link: "/release-notes/v141" },
+                        { label: "v1.4.0", link: "/release-notes/v140" },
+                    ],
+                },
+                {
+                    label: "Appendices",
+                    autogenerate: { directory: "appendices" },
+                },
+                {
+                    label: "Support & Contact",
+                    link: "/support-and-contact/",
+                },
+            ],
+            // customCss: ["@fontsource/roboto", "./src/tailwind.css"],
+            customCss: ["@fontsource/inter", "./src/tailwind.css"],
+        }),
+        tailwind({ applyBaseStyles: false }),
+    ],
 });
